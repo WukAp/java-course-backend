f0059fed8d05c1244c60bb70345731aac6908660
wake <kakoy-to_wuk@mail.ru>
2024-03-03 09:12:07 +0300

    init hw3

--- scrapper/src/test/java/edu/java/client/BotClientTest.java
+++ scrapper/src/test/java/edu/java/client/BotClientTest.java
@@ -1,17 +1,17 @@
-package edu.java.client.botClient;
+package edu.java.client;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import edu.java.DTO.LinkUpdateRequest;
+import java.net.URI;
+import edu.java.client.botClient.BotClient;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.net.URI;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.junit.Assert.assertEquals;
 
 class BotClientTest {
     private static WireMockServer wireMockServer;
@@ -32,7 +32,9 @@ class BotClientTest {
     @Test
     public void putUpdate() {
         BotClient client = new BotClient(wireMockServer.baseUrl());
-       Assertions.assertEquals("link has been updated!",
-           client.putUpdate(new LinkUpdateRequest(new URI("link"), "description!", 1)).block());
+        Assertions.assertEquals(
+            "link has been updated!",
+            client.putUpdate(new LinkUpdateRequest(new URI("link"), "description!", 1)).block()
+        );
     }
 }
